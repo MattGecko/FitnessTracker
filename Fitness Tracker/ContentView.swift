b32967b849114ac49cb9fcc1707d3d28e25e9fbe
Joshinem <joshinem@outlook.com>
2024-08-07 17:00:46 +0100

    Minor edits

--- Fitness Tracker/ContentView.swift
+++ Fitness Tracker/ContentView.swift
@@ -1,15 +1,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var userSettings = UserSettings()  // Initialize UserSettings in the ContentView
-
-    init() {
-        // Check and create UserData.json file if it does not exist when ContentView initializes
-        if DataManager.shared.loadData() == nil {
-            // Optionally, provide default data for the file
-            DataManager.shared.createUserDataFile()
-        }
-    }
+    @StateObject var userSettings = UserSettings()
 
     var body: some View {
         TabView {
@@ -17,10 +9,21 @@ struct ContentView: View {
                 .tabItem {
                     Label("Profile", systemImage: "person.fill")
                 }
-            MealTrackerView(userSettings: userSettings)  // Pass the same userSettings instance
+            MealTrackerView(userSettings: userSettings)
                 .tabItem {
                     Label("Meals", systemImage: "list.bullet")
                 }
+        }
+        .onAppear {
+            checkAndInitializeData()
+        }
+    }
+
+    private func checkAndInitializeData() {
+        // Check and create UserData.json file if it does not exist when ContentView is displayed
+        if DataManager.shared.loadData() == nil {
+            // Optionally, provide default data for the file
+            DataManager.shared.createUserDataFile()
         }
     }
 }
